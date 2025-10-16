d493b4e1e6733cffd956d276df1a87e02ba3b5a6
Alagross <nguyen.albert.k@gmail.com>
2025-10-16 01:50:30 -0400

    Added colors and legends to DataCenterMap.js

--- js/DataCenterMapChart.js
+++ js/DataCenterMapChart.js
@@ -10,7 +10,7 @@ class DataCenterMapChart {
 
     initVis() {
         let vis = this;
-
+        vis.legendValues = [1000, 100000, 500000];
         vis.margin = { top: 20, right: 20, bottom: 20, left: 20 };
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
@@ -46,23 +46,102 @@ class DataCenterMapChart {
             .attr("fill", "#eee")
             .attr("stroke", "#aaa");
 
-        //Color Coding for operators
-        vis.operators = [...new Set(vis.data.map(d => d.operator))];
+        vis.filteredData = vis.data.filter(d =>
+            d.operator && d.operator.trim().toLowerCase() !== "unknown"
+        );
+                
+        vis.operatorCounts = d3.rollup(vis.filteredData, v => v.length, d => d.operator);
+
+        vis.topOperators = Array.from(vis.operatorCounts.entries())
+            .sort((a, b) => d3.descending(a[1], b[1]))
+            .slice(0, 5)
+            .map(d => d[0]);
+
+        vis.displayData = vis.data.map(d => ({
+            ...d,
+            operator_grouped: vis.topOperators.includes(d.operator) ? d.operator : "Other"
+        }));
+        vis.operators = [...vis.topOperators, "Other"];
         vis.colorScale = d3.scaleOrdinal()
             .domain(vis.operators)
-            .range(vis.operators.map((_, i) => d3.interpolateRainbow(i / vis.operators.length)));
+            .range(d3.schemeTableau10.slice(0, 6))
         
         //Legend
         vis.svg_legend = d3.select("#data-center-map-legend").append("svg")
             .attr("width", vis.width + vis.margin.left + vis.margin.right)
-            .attr("height", vis.margin.top)
+            .attr("height", vis.margin.top + 40)
             .append("g")
-            .attr("transform", `translate(${50}, ${0})`);
- 
+            .attr("transform", `translate(${20}, ${0})`);
         
-        
-        vis.wrangleData();
-    }
+        vis.legendGroup = vis.svg_legend.append("g")
+            .attr("class", "legend-group")
+            .attr("transform", `translate(${0}, ${vis.margin.top / 2})`);
+
+        vis.legendGroup.append("circle")
+            .attr("cx", 0)
+            .attr("cy", 20)
+            .attr("r", 5)
+            .attr("fill", "#2c7fb8");
+
+        vis.legendGroup.append("text")
+            .attr("x", 12)
+            .attr("y", 25)
+            .text("= 2,000 square feet")
+            .style("font-size", "14px")
+            .style("fill", "#333");
+
+        vis.legendGroup.append("circle")
+            .attr("cx", 240)
+            .attr("cy", 20)
+            .attr("r", 10)
+            .attr("fill", "#2c7fb8");
+
+        vis.legendGroup.append("text")
+            .attr("x", 260)
+            .attr("y", 25)
+            .text("= 200,000 square feet")
+            .style("font-size", "14px")
+            .style("fill", "#333");
+
+        vis.legendGroup.append("circle")
+            .attr("cx", 500)
+            .attr("cy", 20)
+            .attr("r", 20)
+            .attr("fill", "#2c7fb8");
+
+        vis.legendGroup.append("text")
+            .attr("x", 530)
+            .attr("y", 25)
+            .text("= 20,00,000 square feet")
+            .style("font-size", "14px")
+            .style("fill", "#333");
+
+        vis.colorLegend = vis.svg.append("g")
+            .attr("class", "color-legend")
+            .attr("transform", `translate(${50}, ${50})`); // adjust position as needed
+
+
+        vis.legendItem = vis.colorLegend.selectAll(".legend-item")
+            .data(vis.operators)
+            .enter()
+            .append("g")
+            .attr("class", "legend-item")
+            .attr("transform", (d, i) => `translate(0, ${i * 20})`);
+
+
+        vis.legendItem.append("circle")
+            .attr("r", 6)
+            .attr("fill", d => vis.colorScale(d));
+
+
+        vis.legendItem.append("text")
+            .attr("x", 12)
+            .attr("y", 4)
+            .text(d => d || "Unknown Operator")
+            .style("font-size", "12px")
+            .style("fill", "#333");
+                vis.wrangleData();
+            }
 
     wrangleData() {
         let vis = this;
@@ -82,7 +161,6 @@ class DataCenterMapChart {
 
 
         // TODO: Add tooltips for each circle
-        // TODO: Add Legend for size (?)
         // TODO: Add Filters for data centers of certain size / company
         vis.g.selectAll(".plant-circle")
             .data(vis.displayData)
@@ -98,7 +176,7 @@ class DataCenterMapChart {
                 return coords ? coords[1] : null;
             })
             .attr("r", d => vis.radiusScale(d.sqft))
-            .attr("fill", d => vis.colorScale(d.operator))
+            .attr("fill", d => vis.colorScale(d.operator_grouped))
             .attr("opacity", 0.75)
     }
 }
