e12b8fffa3415fd3ca2a95e466f5e82e877bc753
Tyler-Parker <tcparker24@gmail.com>
2025-11-14 14:38:00 -0500

    fixed arrows

--- js/AccuracyAgeChart.js
+++ js/AccuracyAgeChart.js
@@ -35,7 +35,7 @@ class AccuracyAgeChart {
             vis.wrangleData()
         });
 
-        vis.margin = { top: 10, right: 50, bottom: 10, left: 50 };
+        vis.margin = { top: 50, right: 50, bottom: 50, left: 50 };
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
@@ -113,13 +113,42 @@ class AccuracyAgeChart {
                 .attr("fill", vis.colors[i % 2]);
 
             vis.chartGroup.append("text")
-                .attr("x", -1 * (vis.maxRadius - i * vis.ringThickness) + 15)  // 5px padding from the left edge
+                .attr("x", i !== 4 ? -1 * (vis.maxRadius - i * vis.ringThickness) + 10 : -1 * (vis.maxRadius - i * vis.ringThickness) + 25)
                 .attr("y", 0)
                 .attr("text-anchor", "start")
                 .attr("alignment-baseline", "middle")
-                .attr("fill", i % 2 === 0 ? "#ffffff" : "#c83737ff") // contrast
-                .attr("font-size", "5px")
+                .attr("fill", i % 2 === 0 ? "#ffffff" : "#c83737ff")
+                .attr("font-size", "8px")
                 .text(`${25 * (i)}%`);
+
+            vis.chartGroup.append("text")
+                .attr("x", (vis.maxRadius - i * vis.ringThickness) - 10)
+                .attr("y", 0)
+                .attr("text-anchor", "end")
+                .attr("alignment-baseline", "middle")
+                .attr("fill", i % 2 === 0 ? "#ffffff" : "#c83737ff")
+                .attr("font-size", "8px")
+                .text(`${i !== 4 ? 25 * i + '%' : ''}`);
+
+            vis.chartGroup.append("text")
+                .attr("x", 0)
+                .attr("y", -(vis.maxRadius - i * vis.ringThickness) + 16)
+                .attr("text-anchor", "middle")
+                .attr("alignment-baseline", "middle")
+                .attr("fill", i % 2 === 0 ? "#ffffff" : "#c83737ff")
+                .attr("font-size", "8px")
+                .text(`${i !== 4 ? 25 * i + '%' : ''}`);
+
+            vis.chartGroup.append("text")
+                .attr("x", 0)
+                .attr("y", (vis.maxRadius - i * vis.ringThickness) - 16)
+                .attr("text-anchor", "middle")
+                .attr("alignment-baseline", "middle")
+                .attr("fill", i % 2 === 0 ? "#ffffff" : "#c83737ff")
+                .attr("font-size", "8px")
+                .text(`${i !== 4 ? 25 * i + '%' : ''}`);
+
+
         }
 
 
@@ -144,6 +173,7 @@ class AccuracyAgeChart {
         let vis = this;
 
         vis.chartGroup.selectAll("image").remove();
+        vis.chartGroup.selectAll("text.accuracy-label").remove();
 
         let dartCenterXPos = 0;
         let dartCenterYPos = 0;
@@ -169,6 +199,8 @@ class AccuracyAgeChart {
         let realStartX = realTargetX + 600 * Math.cos(realAngle);
         let realStartY = realTargetY + 600 * Math.sin(realAngle);
 
+        let scale = 0.75
+
         vis.chartGroup.append("image")
             .attr("xlink:href", "resources/dart_ai_accuracy_color.png")
             .attr("width", 60)
@@ -192,6 +224,22 @@ class AccuracyAgeChart {
             .attr("y", aiTargetY - 150)
             .attr("transform", `rotate(${aiAngleDeg}, ${aiTargetX}, ${aiTargetY})`);
 
+        vis.chartGroup.append("text")
+            .attr("class", "accuracy-label")
+            .text(`${aiAccuracy}%`)
+            .attr("x", aiStartX)
+            .attr("y", aiStartY - 160)
+            .attr("text-anchor", "middle")
+            .attr("fill", "#d1814b")
+            .attr("font-size", "14px")
+            .attr("transform", `rotate(${aiAngleDeg}, ${aiStartX}, ${aiStartY})`)
+            .transition()
+            .duration(300)
+            .ease(d3.easeCubicOut)
+            .attr("x", aiTargetX)
+            .attr("y", aiTargetY - 160)
+            .attr("transform", `rotate(${aiAngleDeg}, ${aiTargetX}, ${aiTargetY})`);
+
         vis.chartGroup.append("image")
             .attr("xlink:href", "resources/dart_human_accuracy_color.png")
             .attr("width", 60)
@@ -213,6 +261,22 @@ class AccuracyAgeChart {
             .ease(d3.easeCubicOut)
             .attr("x", realTargetX - 30)
             .attr("y", realTargetY - 150)
+            .attr("transform", `rotate(${realAngleDeg}, ${realTargetX}, ${realTargetY})`);
+
+        vis.chartGroup.append("text")
+            .attr("class", "accuracy-label")
+            .text(`${realAccuracy}%`)
+            .attr("x", realStartX)
+            .attr("y", realStartY - 160)
+            .attr("text-anchor", "middle")
+            .attr("fill", "#4b9cd3")
+            .attr("font-size", "14px")
+            .attr("transform", `rotate(${realAngleDeg}, ${realStartX}, ${realStartY})`)
+            .transition()
+            .duration(300)
+            .ease(d3.easeCubicOut)
+            .attr("x", realTargetX)
+            .attr("y", realTargetY - 160)
             .attr("transform", `rotate(${realAngleDeg}, ${realTargetX}, ${realTargetY})`);
     }
 
